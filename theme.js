7400aae19140f12e7678494d77b2f563d357a597
codingYank <tyler.schmidt99@gmail.com>
2023-08-12 15:31:51 -0400

    Add github link

--- theme.js
+++ theme.js
@@ -1,11 +1,19 @@
 const savedTheme = localStorage.getItem("theme") || "light"
 const themeToggle = document.querySelector("#theme")
+const githubIcon = document.getElementById("github-icon")
+const githubIconWhite = document.getElementById("github-icon-white")
 let checked = localStorage.getItem("toggleCheckbox")
 
 if (checked === "true") {
   document.body.classList.remove("theme-light", "theme-dark")
   document.body.classList.add("theme-dark")
+  githubIcon.classList.remove("hide")
+  githubIconWhite.classList.add("hide")
+
   themeToggle.checked = true
+} else {
+  githubIcon.classList.add("hide")
+  githubIconWhite.classList.remove("hide")
 }
 
 themeToggle.addEventListener("change", (e) => {
@@ -13,9 +21,13 @@ themeToggle.addEventListener("change", (e) => {
   if (themeToggle.checked) {
     document.body.classList.remove("theme-light", "theme-dark")
     document.body.classList.add("theme-dark")
+    githubIcon.classList.remove("hide")
+    githubIconWhite.classList.add("hide")
     localStorage.setItem("toggleCheckbox", "true")
   } else {
     document.body.classList.remove("theme-light", "theme-dark")
     document.body.classList.add("theme-light")
+    githubIcon.classList.add("hide")
+    githubIconWhite.classList.remove("hide")
   }
 })
